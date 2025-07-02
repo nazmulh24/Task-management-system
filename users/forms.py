f335b62faac1b3b37ce406ed52c0814d5b3d66a3
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-02 14:40:08 +0600

    10.4 & 10.5 - Forms Field & Non-Field Errors

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+import re
 
 
 class RegisterForm(UserCreationForm):
@@ -19,3 +21,54 @@ class RegisterForm(UserCreationForm):
 
         for fieldName in ["username", "password1", "password2"]:
             self.fields[fieldName].help_text = None
+
+
+class CustomRegisterForm(forms.ModelForm):
+    pass1 = forms.CharField(widget=forms.PasswordInput, label="Password")
+    pass2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "pass1",
+            "pass2",
+        ]
+
+    def clean_email(self):  # ---------> Duplicate Email Error
+        email = self.cleaned_data.get("email")
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email Already Exists.")
+
+        return email
+
+    def clean_pass1(self):  # -------------------> Field Error
+        pass1 = self.cleaned_data.get("pass1")
+        errors = []
+
+        if len(pass1) < 8:
+            errors.append("Password must be at least 8 characters.")
+        if not re.search(r"[!@#$%^&*()\-_=+]", pass1):
+            errors.append("Password must contain at least one special character.")
+
+        if errors:
+            raise forms.ValidationError(errors)
+
+        return pass1
+
+    def clean(self):  # ---------------------> Non Field Error
+        cleaned_data = super().clean()
+        pass1 = cleaned_data.get("pass1")
+        pass2 = cleaned_data.get("pass2")
+
+        if pass1 != pass2:
+            raise forms.ValidationError("Password do not match.")
+
+        return cleaned_data
+
+
+#
--- users/views.py
+++ users/views.py
@@ -1,27 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from users.forms import RegisterForm
+from users.forms import RegisterForm, CustomRegisterForm
 
 
 def sign_up(request):
     if request.method == "POST":
-        form = RegisterForm(request.POST)
+        form = CustomRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-        #     username = form.cleaned_data.get("username")
-        #     pass1 = form.cleaned_data.get("password1")
-        #     pass2 = form.cleaned_date.get("password2")
-
-        #     if pass1 == pass2:
-        #         User.objects.create(username=username, password=pass1)
-        #     else:
-        #         print("Password are not same")
-        # else:
-        #     print("Form is not valid...")
 
     else:
-        form = RegisterForm()
+        form = CustomRegisterForm()
 
     context = {"form": form}
     return render(request, "registration/register.html", context)
