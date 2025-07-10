70685a5e6313e3293b709949442a80d45c4edb6e
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-10 15:27:52 +0600

    16.3 ProfileView using TemplateView

--- users/urls.py
+++ users/urls.py
@@ -8,15 +8,16 @@ from users.views import (
     assign_role,
     create_group,
     group_list,
+    CustomLogin,
+    ProfileView,
 )
-from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path("sign-up/", sign_up, name="sign-up"),
     # path("sign-in/", sign_in, name="sign-in"),
     path(
         "sign-in/",
-        LoginView.as_view(
+        CustomLogin.as_view(
             # template_name="registration/login.html",
         ),
         name="sign-in",
@@ -27,4 +28,5 @@ urlpatterns = [
     path("admin/<int:user_id>/assign-role/", assign_role, name="assign-role"),
     path("admin/create-group/", create_group, name="create-group"),
     path("admin/group-list/", group_list, name="group-list"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -7,6 +7,8 @@ from django.contrib import messages
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db.models import Prefetch
+from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
 
 
 def is_admin(user):
@@ -49,6 +51,15 @@ def sign_in(request):
         "form": form,
     }
     return render(request, "registration/login.html", context)
+
+
+class CustomLogin(LoginView):
+    form_class = LoginForm
+
+    def get_success_url(self):
+        next_url = self.request.GET.get("next")  # --------> Problem Here...
+        print(next_url)
+        return next_url if next_url else super().get_success_url()
 
 
 @login_required
@@ -137,3 +148,19 @@ def group_list(request):
     groups = Group.objects.prefetch_related("permissions").all()
 
     return render(request, "admin/group_list.html", {"groups": groups})
+
+
+class ProfileView(TemplateView):
+    template_name = "accounts/profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        context["username"] = user.username
+        context["email"] = user.email
+        context["name"] = user.get_full_name()
+        context["member_since"] = user.date_joined
+        context["last_login"] = user.last_login
+
+        return context
